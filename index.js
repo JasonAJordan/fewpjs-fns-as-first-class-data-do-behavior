0de0233f46ab5644d17fc45dd332074c5f58202e
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-12-09 20:40:44 -0500

    asdf

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string){
+
+  // let time = new Date()
+
+  // let hours = time.getHours()
+
+  let hours = parseInt(string, 10)
+
+  if (hours < 12){
+    return "Good Morning"
+  } else if ( hours < 15) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+
+}
+
+function displayMessage(msg) {
+
+  document.getElementById("greeting").innerText = msg
+
+}
